06bb50ec8d64cbc909813aebe061f1d47ebe5a5e
Mikhail Lipilin <mikhail.lipilin@balance-pl.ru>
2019-01-29 12:55:47 +0300

    initial commit

---
+++ src/app.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import './app.css';
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <h1 className="App__title">React App Template</h1>
+            </div>
+        );
+    }
+}
+
+export default App;
