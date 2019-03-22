53930588d725af101c6f43ef14c50efaf1b055d2
Mikhail Lipilin <mikhail.lipilin@balance-pl.ru>
2019-03-22 16:55:42 +0300

    assets: styles, images, fonts

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 import { sum } from './lib';
 
+import './styles/styles.css';
+
 console.log(sum(1, 2));
+
+let img = new Image();
+img.onload = () => {
+    document.body.appendChild(img);
+};
+img.src = require('./images/logo.png');
